e056443d4f9815829bd9e3a5dff72ea4d209d604
Hedzer van Dijk <hedzer@triaspect.nl>
2020-05-16 14:03:35 +0200

    posts migration toegevoegd

--- MyContext.cs
+++ MyContext.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFModeling.FluentAPI.Required
@@ -21,5 +22,16 @@ namespace EFModeling.FluentAPI.Required
     {
         public int BlogId { get; set; }
         public string Url { get; set; }
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
     }
 }
