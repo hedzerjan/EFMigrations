74c980f92fe421f059893d7ba437e3d80c6a6eff
Hedzer van Dijk <hedzer@triaspect.nl>
2020-05-16 14:14:39 +0200

    nog wat code toegevoegd voor testjes

--- Program.cs
+++ Program.cs
@@ -11,15 +11,38 @@ namespace EFMigrations
             using (var db = new MyContext())
             {
                 // Create
-                Console.WriteLine("Inserting a new blog");
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.SaveChanges();
+                // Console.WriteLine("Inserting a new blog");
+                // db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                // db.SaveChanges();
 
                 // Read
-                Console.WriteLine("Querying for a blog");
+                // Console.WriteLine("Querying for a blog");
+                // var blog = db.Blogs
+                //     .OrderBy(b => b.BlogId)
+                //     .First();
+
+                // /// Update
+                // Console.WriteLine("Updating the blog and adding a post");
+                // blog.Url = "https://devblogs.microsoft.com/dotnet";
+                // blog.Posts.Add(
+                //     new Post
+                //     {
+                //         Title = "Hello World",
+                //         Content = "I wrote an app using EF Core!"
+                //     });
+                // db.SaveChanges();
+
+                // Delete
                 var blog = db.Blogs
-                    .OrderBy(b => b.BlogId)
-                    .First();
+                    .Where(p => p.BlogId == 2);
+                Console.WriteLine($"Gevonden blog: {blog}");
+
+                if (blog.Count() == 1)
+                {
+                    Console.WriteLine("Delete the blog");
+                    db.Remove(blog.First());
+                    db.SaveChanges();
+                }
             }
         }
     }
